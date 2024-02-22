a932cd004be26058ae2bf033ca04c85bbbef44c0
Restyled.io <commits@restyled.io>
2024-02-22 18:39:01 +0000

    Restyled by prettier

--- src/controls/legend/imagesource.js
+++ src/controls/legend/imagesource.js
@@ -1,12 +1,9 @@
 const getSource = (style) => {
   const image = style.image || style.custom.image;
-  if (!image)
-    return null;
+  if (!image) return null;
   const src = image.src || null;
-  if (!src)
-    throw new Error('image style must have src');
-  else
-    return src;
+  if (!src) throw new Error("image style must have src");
+  else return src;
 };
 
 // return the image source from an origo style
